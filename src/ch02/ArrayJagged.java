fc9532749a73d2b9358e564f3005138533b2b978
이지윤 <wbs0810@gmail.com>
2024-05-02 19:04:40 +0900

    2

---
+++ src/ch02/ArrayJagged.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class ArrayJagged {
+
+	public static void main(String[] args) {
+		var list = new int[][] {
+			{ 1, 2, 3 },
+			{ 4, 5, 6 },
+			{ 7, 8, 9 }
+		};
+
+		System.out.println(list[0][1]); //2
+	}
+
+}
---
+++ src/ch02/Const.java
@@ -0,0 +1,14 @@
+package ch02;
+
+public class Const {
+
+	public static void main(String[] args) {
+		final double TAX = 1.08;
+		int price = 1000;
+		double sum = price * TAX;
+		System.out.println(sum);
+
+		// 결과 : 1080.0
+	}
+
+}
